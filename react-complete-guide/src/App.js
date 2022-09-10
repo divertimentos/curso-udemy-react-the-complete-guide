600ba4a94d3b6f8804b739595e2e397369c1b9ab
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2022-09-10 15:52:07 -0300

    Fixes the fucking eslint problems

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ExpenseItem from './components/ExpenseItem';
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
