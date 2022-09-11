103166c029e8df3fb757354c63cb6bd66d77939c
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2022-09-11 16:43:35 -0300

    Improves code

--- react-complete-guide/src/components/Expenses.js
+++ react-complete-guide/src/components/Expenses.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 function Expenses(props) {
   const { expenses } = props;
-  console.dir(expenses);
 
   return (
     <div className="expenses">
       {expenses.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
