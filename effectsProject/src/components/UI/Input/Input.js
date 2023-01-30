1a517f2276c428dcb94d4d80aba88f4bb82c15e4
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2023-01-30 14:24:11 -0300

    Finishes Section 10

--- effectsProject/src/components/UI/Input/Input.js
+++ effectsProject/src/components/UI/Input/Input.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from "react";
 
-import classes from './Input.module.css';
+import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+
   return (
-
     <div
-      className={`${classes.control} ${props.isValid === false ? classes.invalid : ''}`}>
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       <input
-        type={props.type || 'input'}
+        ref={inputRef}
+        type={props.type || "input"}
         value={props.value}
         id={props.id}
         onChange={props.onChange}
         onBlur={props.onBlur}
       />
     </div>
-  )
-}
+  );
+});
 
 export default Input;
