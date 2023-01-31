07b0ae34078d59e85690cac4a91369a0c58ea87d
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2023-01-31 16:30:12 -0300

    Adding a Header component

--- foodOrderApp/src/App.js
+++ foodOrderApp/src/App.js
@@ -1,9 +1,7 @@
+import Header from "./components/Layout/Header";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
