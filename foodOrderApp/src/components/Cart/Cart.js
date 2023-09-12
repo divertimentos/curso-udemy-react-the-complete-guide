789f6648815c7be1f93594e400ff580bc5c76187
ggteixeira <24235344+ggteixeira@users.noreply.github.com>
2023-09-11 22:41:10 -0300

    Things

--- foodOrderApp/src/components/Cart/Cart.js
+++ foodOrderApp/src/components/Cart/Cart.js
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
